081e01c7910949f2053c357d9ba26f8fde200e1f
cpysden-coder <chris_pysden@mac.com>
2022-01-29 09:52:15 -0800

    updated to classes from prototypes

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,39 +1,40 @@
 // objects
 
-function Objects(){
-
+class Objects {
+    constructor() {
+    }
+    spaceship(x, y, spaceship_image) {
+        this.x = x;
+        this.y = y;
+        this.width = 34;
+        this.height = 28;
+        this.spaceship_image = spaceship_image;
+        this.spaceship_image.src = "assets/images/ship.png";
+        return this;
+    }
+    bullet(x, y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+    ufo(x, y, line, column, ufo_image) {
+        this.x = x;
+        this.y = y;
+        this.line = line; //effectively the row of the ufos
+        this.column = column;
+        this.width = 32;
+        this.height = 24;
+        this.ufo_image = ufo_image;
+        this.ufo_image.src = "assets/images/ufo.png";
+        return this;
+    }
+    bomb(x, y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
 };
 
-Objects.prototype.spaceship = function(x,y,spaceship_image){
-    this.x = x;
-    this.y = y;
-    this.width = 34;
-    this.height = 28;
-    this.spaceship_image = spaceship_image;
-    this.spaceship_image.src = "assets/images/ship.png"
-    return this;
-};
 
-Objects.prototype.bullet = function(x,y){
-    this.x = x;
-    this.y = y;
-    return this;
-};
 
-Objects.prototype.ufo = function(x, y, line, column, ufo_image){
-    this.x = x;
-    this.y = y;
-    this.line = line; //effectively the row of the ufos
-    this.column = column;
-    this.width = 32;
-    this.height = 24;
-    this.ufo_image = ufo_image;
-    this.ufo_image.src = "assets/images/ufo.png";
-    return this;
-}
 
-Objects.prototype.bomb = function(x, y){
-    this.x = x;
-    this.y = y;
-    return this;
-}
